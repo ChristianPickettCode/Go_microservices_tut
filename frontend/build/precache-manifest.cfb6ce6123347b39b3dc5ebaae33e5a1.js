bcca3fbae5aa2ae57bf77163f93aadfbe94142cc
Chris Pickett <chris@Chriss-MacBook-Pro.local>
2022-11-12 00:17:33 -0500

    ep9

---
+++ frontend/build/precache-manifest.cfb6ce6123347b39b3dc5ebaae33e5a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6078f677781f7b916060b82b62140c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6a10dc44cd05bd3fb9",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d2b0328a163cd9aa7cac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a6a10dc44cd05bd3fb9",
+    "url": "/static/js/2.a16bd270.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a16bd270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b0328a163cd9aa7cac",
+    "url": "/static/js/main.5a0a9391.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
